2abd88c6363561e6f63854d55dab7c7d0029bb35
fishstiz <fishstickz7777@gmail.com>
2025-08-29 05:10:18 +0800

    fixes

--- common/src/main/java/io/github/fishstiz/cursors_extended/gui/screen/panel/DebugOptionsPanel.java
+++ common/src/main/java/io/github/fishstiz/cursors_extended/gui/screen/panel/DebugOptionsPanel.java
@@ -9,8 +9,8 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class DebugOptionsPanel extends AbstractOptionsPanel {
-    private static final String ISSUES_LINK = "https://github.com/fishstiz/minecraft-cursor/issues";
-    private static final String WIKI_LINK = "https://fishstiz.github.io/minecraft-cursor-wiki/resource-pack/getting-started";
+    private static final String ISSUES_LINK = "https://github.com/fishstiz/cursors_extended/issues";
+    private static final String WIKI_LINK = "https://fishstiz.github.io/cursors_extended-wiki/resource-pack/getting-started";
     private static final Component INSPECT_TEXT = Component.translatable("cursors_extended.options.debug.inspect");
     private static final Component REPORT_ISSUES_TEXT = Component.translatable("cursors_extended.options.debug.report_issues");
     private static final Component OPEN_WIKI_TEXT = Component.translatable("cursors_extended.options.debug.open_wiki");
@@ -32,10 +32,12 @@ public class DebugOptionsPanel extends AbstractOptionsPanel {
                 true
         );
 
-        this.optionsList.addWidget(new ButtonWidget(
+        ButtonWidget wikiButton = new ButtonWidget(
                 this.index(OPEN_WIKI_TEXT),
                 ConfirmLinkScreen.confirmLink(this.getScreen(), WIKI_LINK, true)
-        ));
+        );
+        wikiButton.active = false;
+        this.optionsList.addWidget(wikiButton);
         this.optionsList.addWidget(new ButtonWidget(
                 this.index(REPORT_ISSUES_TEXT),
                 ConfirmLinkScreen.confirmLink(this.getScreen(), ISSUES_LINK, true)
--- common/src/main/java/io/github/fishstiz/cursors_extended/mixin/cursorprovider/AbstractSliderButtonMixin.java
+++ common/src/main/java/io/github/fishstiz/cursors_extended/mixin/cursorprovider/AbstractSliderButtonMixin.java
@@ -2,16 +2,22 @@ package io.github.fishstiz.cursors_extended.mixin.cursorprovider;
 
 import com.mojang.blaze3d.platform.cursor.CursorType;
 import com.mojang.blaze3d.platform.cursor.CursorTypes;
+import io.github.fishstiz.cursors_extended.CursorsExtended;
 import io.github.fishstiz.cursors_extended.cursor.CursorProvider;
 import io.github.fishstiz.cursors_extended.cursor.CursorTypesExt;
-import io.github.fishstiz.cursors_extended.util.CursorTypeUtil;
 import net.minecraft.client.gui.components.AbstractSliderButton;
 import net.minecraft.client.gui.components.AbstractWidget;
 import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(AbstractSliderButton.class)
 public abstract class AbstractSliderButtonMixin extends AbstractWidget implements CursorProvider {
+    @Shadow
+    private boolean dragging;
+
     protected AbstractSliderButtonMixin(int x, int y, int width, int height, Component message) {
         super(x, y, width, height, message);
     }
@@ -20,15 +26,26 @@ public abstract class AbstractSliderButtonMixin extends AbstractWidget implement
     public CursorType cursors_extended$cursorType(double mouseX, double mouseY) {
         if (this.isHovered()) {
             if (this.isActive()) {
-                if (this.isFocused() && (CursorTypeUtil.isLeftClickHeld())) {
-                    return CursorTypesExt.RESIZE_EW_HOLD;
-                } else {
-                    return CursorTypes.POINTING_HAND;
-                }
+                return this.dragging ? CursorTypesExt.RESIZE_EW_HOLD : CursorTypes.POINTING_HAND;
             } else {
                 return CursorTypes.NOT_ALLOWED;
             }
         }
         return CursorTypes.ARROW;
+    }
+
+    @ModifyArg(method = "renderWidget", at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/client/gui/GuiGraphics;requestCursor(Lcom/mojang/blaze3d/platform/cursor/CursorType;)V"
+    ))
+    private CursorType setCursor(CursorType cursorType) {
+        if (CursorsExtended.CONFIG.isLegacyMode()) {
+            if (this.isActive()) {
+                return this.dragging ? CursorTypesExt.RESIZE_EW_HOLD : CursorTypes.POINTING_HAND;
+            } else {
+                return CursorTypes.NOT_ALLOWED;
+            }
+        }
+        return cursorType;
     }
 }
